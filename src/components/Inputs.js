35e18f9ea251d93372eff0d14f57228324bd34bc
Andis <mnemosx@gmail.com>
2022-01-27 15:31:58 +0200

    remove unused usestate

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './Inputs.module.scss'
 import DatePicker from 'react-datepicker'
 import { useDispatch, useSelector } from 'react-redux'
